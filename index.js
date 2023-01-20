dc5b5e3b47ad347a8c6ff157f1b6bca311a94305
danielforkner <daniel.forkner@gmail.com>
2023-01-20 09:10:46 -0500

    connect the client

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
+const client = require('./db/client');
+
+app.listen(3000, async () => {
   console.log('listening on port 3000');
+  try {
+    await client.connect();
+    console.log('Connected to the db!');
+  } catch (error) {
+    console.error(error);
+    console.log("Couldn't connect to the db!");
+  }
 });
